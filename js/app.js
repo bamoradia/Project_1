711bfa2a3543e5ef67bd11897b3e536f5934c166
Badal Moradia <badal@i360llc.com>
2018-07-03 15:45:53 -0500

    adjust odds of spawning coin and mushrooms

--- js/app.js
+++ js/app.js
@@ -15,9 +15,9 @@ let gameOver = false; //set game over as false
 let enemyCount = 0; //used to only allow 1 new enemy during spawn point
 let score = 0; //keep track of user score
 const $score = $('#score');
-let gameSpeedTracker = 0;
-let enemyDistance = 250;
-let obstacleDistance = 100;
+let gameSpeedTracker = 0; //count to make sure game speed is only increased once
+let enemyDistance = 250;//distance at which new enemies spawn
+let obstacleDistance = 100;//distance at which new obstacles may spawn
 const $button = $('#pause');
 
 
@@ -38,15 +38,14 @@ pipeImage.onload = function (X, Y, width, height) {//draw block image
   ctx.drawImage(pipeImage, 0, 160, 32, 32, X, Y, width, height);
 }
 
+
 let coinImage = new Image();
 coinImage.src = '/../Users/bamoradia/Documents/funky-ducks/Project_1/js/sprites/items_sheet.png';
-
-let mushroomImage = new Image();
-mushroomImage.src = '/../Users/bamoradia/Documents/funky-ducks/Project_1/js/sprites/items_sheet.png';
 
 coinImage.onload = function (X, Y) {//draw the ground image
   ctx.drawImage(coinImage, 3, 97, 15, 15, X, Y - 25, 30, 20) 
 }
+
 
 let characterImage = new Image();
 characterImage.src = '/../Users/bamoradia/Documents/funky-ducks/Project_1/js/sprites/luigi_sprite_sheet.png';
@@ -55,6 +54,7 @@ characterImage.onload = function (X, Y, width, height) {//draw block image
   ctx.drawImage(characterImage, 210, 0, 15, 15, X, Y, width, height);
 }
 
+
 let blockImage = new Image();
 blockImage.src = '/../Users/bamoradia/Documents/funky-ducks/Project_1/js/sprites/blocks_sheet.png';
 
@@ -62,11 +62,20 @@ blockImage.onload = function (X, Y, width, height) {//draw block image
   ctx.drawImage(blockImage, 20, 1, 15, 15, X, Y, width, height);
 }
 
+
 let enemyImage = new Image();
 enemyImage.src = '/../Users/bamoradia/Documents/funky-ducks/Project_1/js/sprites/enemy_sheet.png';
 
 enemyImage.onload = function (X, Y, width, height) {//draw block image
   ctx.drawImage(enemyImage, 0, 16, 17, 15, X, Y, width, height);
+}
+
+
+let mushroomImage = new Image();
+mushroomImage.src = '/../Users/bamoradia/Documents/funky-ducks/Project_1/js/sprites/items_sheet.png';
+
+mushroomImage.onload = function (X, Y) {//draw the ground image
+  ctx.drawImage(mushroomImage, 15, 3, 17, 14, X, Y - 20, 25, 20) 
 }
 
 //make an array with all the current characters 
@@ -86,11 +95,6 @@ const mainPlayer = {//setting up x, y, height and width as well as yVelocity
   lastY: 335, //tracks the Y direction that mario is currently going
   moving: false,
 	draw(){//redraw the main character
-		// ctx.beginPath();
-  //   ctx.rect(this.x, this.y, this.width, this.height);
-  //   ctx.fillStyle = "green";
-  //   ctx.fill();
-  //   ctx.closePath();
   characterImage.onload(this.x, this.y, this.width, this.height);
   },
 }
@@ -107,13 +111,7 @@ class Enemy {//enemy Class, will be used to make multiple enemies
     this.xVelocity = -1; //all enemies will move in relation to the background intially
   }
   draw() {
-    // ctx.beginPath();
-    // ctx.rect(this.x, this.y, this.width, this.height);
-    // ctx.fillStyle = "yellow";
-    // ctx.fill();
-    // ctx.closePath();
     enemyImage.onload(this.x, this.y, this.width, this.height)
-
   }
 }
 
@@ -130,19 +128,21 @@ class Obstacle {//class of obstacles to be called inside of another function
     this.type = type;
   }
   draw () {
-    if(this.type === 'largePipe' || this.type === 'smallPipe') {
+    if(this.type === 'largePipe' || this.type === 'smallPipe') {//if obstacle is pipe
       pipeImage.onload(this.x, this.y, this.width, this.height);
     }
   }
   drawCoin() {
-    if(this.hasItem === true && this.item == 'coin'){
+    if(this.hasItem === true && this.item == 'coin'){//if obstacle has a coin
       coinImage.onload(this.x, this.y);
     }
   }
   drawMushroom() {
-
+    if(this.hasItem === true && this.item == 'mushroom'){//if obstacle has a coin
+      mushroomImage.onload(this.x, this.y);
+    }
   }
-  drawBlock() {
+  drawBlock() {//if the obstacle is a block
     if(this.type === 'block') {
       blockImage.onload(this.x, this.y, this.width, this.height);
     }
@@ -161,6 +161,7 @@ function clearCanvas() {//clears the canvas and redraws the ground image
     if(mainPlayer.moving ) {
       allObstacles[i].x = allObstacles[i].x - gameSpeed;
     }
+    //draw all items/obstacles types
     allObstacles[i].draw();
     allObstacles[i].drawCoin();
     allObstacles[i].drawBlock();
@@ -170,8 +171,8 @@ function clearCanvas() {//clears the canvas and redraws the ground image
 }
 
 
-const gumba = new Enemy();
-const wall = new Obstacle(275, 345, 20, 30, false, '', 'smallPipe');
+const gumba = new Enemy();//declare first enemy
+const wall = new Obstacle(275, 345, 20, 30, false, '', 'smallPipe');//declare first obstacle
 
 allEnemies[0] = gumba; 
 allObstacles[0] = wall;
@@ -185,9 +186,8 @@ const gravity = (object) => {
 		//do nothing
 	} else {
      object.yVelocity += 0.45;//the "acceleration" due to gravity
-     object.lastY = object.y;
+     object.lastY = object.y;//record the current Y position
      object.y = object.y + object.yVelocity; //update the position of the player after gravity 
-      //the velocity of the object increases every tick
 	}
 	return
 }
@@ -203,10 +203,13 @@ const standingOnObject = (character) => { //checks if the object is standing on 
     character.yVelocity = 0; //set the character's yVelocity to 0
     character.y =  allObstacles[i].y - character.height - 1; //draw the character to just above the ground
     if(allObstacles[i].hasItem === true && allObstacles[i].item === 'coin'){
+      //if obstacle had a coin add 200 points to score
       score += 200;
       $score.text(`Score: ${score}`)
+    } else if(allObstacles[i].hasItem === true && allObstacles[i].item === 'mushroom') {
+      console.log('collected a mushroom')
     }
-    allObstacles[i].hasItem = false;
+    allObstacles[i].hasItem = false;//change obstacle to no longer have item
     jumpCount = 0; //reset the jump counter
     return true
     }
@@ -233,7 +236,7 @@ const moveEnemies = () => { //function to move all enemies based on their xVeloc
   }
 }
 
-
+//function to clear all enemies and obstacles from memory that have left the display
 const clearEverything = () => {
   for(let i = 0; i < allEnemies.length; i++) {
     if(allEnemies[i].x + allEnemies[i].width <= 0) {
@@ -250,8 +253,6 @@ const clearEverything = () => {
 
 
 //function to check for interference between enemy object and main player. To be used for death condition for either character
-
-//need to adjust so that a top interference kills the enemy otherwise main character is killed
 const checkForInterference = () => {
   for(let i = 0; i < allObstacles.length; i++) {//checks for interference will all objects with main player
     if((mainPlayer.y >= allObstacles[i].y && //checks interference on left edge with player
@@ -262,8 +263,8 @@ const checkForInterference = () => {
       mainPlayer.x + mainPlayer.width > allObstacles[i].x -3 &&
       leftKeyPress == false) {
 
-      mainPlayer.moving = false;
-      mainPlayer.x = allObstacles[i].x - mainPlayer.width - 2;
+      mainPlayer.moving = false; //change player moving to false
+      mainPlayer.x = allObstacles[i].x - mainPlayer.width - 2; //player to left edge of obstacle
 
     } else if ((mainPlayer.y >= allObstacles[i].y &&  //checks interference on right edge with player
       mainPlayer.y <= allObstacles[i].y + allObstacles[i].height ||
@@ -273,7 +274,7 @@ const checkForInterference = () => {
       mainPlayer.x > allObstacles[i].x + allObstacles[i].width - 3 && 
       rightKeyPress == false)
     {
-      mainPlayer.x = allObstacles[i].x + allObstacles[i].width + 3;
+      mainPlayer.x = allObstacles[i].x + allObstacles[i].width + 3; //move player to right edge of obstacle
     }
   }  
   for(let i = 0; i < allEnemies.length; i++) {//checking interference between player and enemies
@@ -377,7 +378,7 @@ const movePlayer = () => {
   }
 
   if(leftKeyPress == true && mainPlayer.x > 0) { //listens for the left press
-    mainPlayer.x -= gameSpeed; // the player moves 2.5px to the left
+    mainPlayer.x -= gameSpeed; // the player moves to the left
     mainPlayer.lastY = mainPlayer.y;
   }
 
@@ -425,14 +426,39 @@ const makeObstacles = () => {
     }
   } else if( odds < .75) {//make floating blocks
     let blockCount = 1
-    const block = new Obstacle(600, 250, 25, 25, false, '', 'block');
-    allObstacles.push(block);
+    const itemOdds = Math.random();
+    let block;
+    //make items based on random odds
+    if(itemOdds < 0.02) {
+      block = new Obstacle(600, 250, 25, 25, true, 'mushroom', 'block');
+      console.log('made mushroom block')
+    } else if(itemOdds < 0.1){
+      block = new Obstacle(600, 250, 25, 25, true, 'coin', 'block');
+      console.log('made coin block')
+    } else {
+      block = new Obstacle(600, 250, 25, 25, false, '', 'block');
+    }
+
+    allObstacles.push(block); //add obstacle to the obstacles array
+
     let blockGroupOdds = Math.random();
     while(blockGroupOdds < 0.4) {//make groups of blocks
-      const block1 = new Obstacle(600+25*blockCount, 250, 25, 25, false, '', 'block')
+      console.log('made blockgroup')
+      let block1
+      const itemOdds1 = Math.random();
+      if(itemOdds1 < 0.005) {
+        block1 = new Obstacle(600 + 25 * blockCount, 250, 25, 25, true, 'mushroom', 'block');
+        console.log('made blockgroup mushroom')
+      } else if(itemOdds1 < 0.1){
+        block1 = new Obstacle(600 + 25 * blockCount, 250, 25, 25, true, 'coin', 'block');
+        console.log('made blockgroup coin')
+      } else {
+        block1 = new Obstacle(600 + 25 * blockCount, 250, 25, 25, false, '', 'block');
+      }
+
       allObstacles.push(block1);
       blockCount++;
-      blockGroupOdds = Math.random();
+      blockGroupOdds = Math.random(); 
     }
 
   }
@@ -494,13 +520,6 @@ function animateCanvas() {
 
 
  animateCanvas();
-
-
-if(gameOver == true) {
-  console.log('The game is over!')
-}
-
-
 
 //just used to prevent overheating and overusing resources
 //remove for actual game
